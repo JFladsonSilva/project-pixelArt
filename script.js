219c99c7a071473f3ad4aec4a02ecf3bc76613bf
JFladsonSIlva <fladsonsilva1@gmail.com>
2022-10-04 18:55:30 -0300

    funções de cores aleatórias criadas com sucesso aparentemente

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ palletB.style.backgroundColor = 'blue'
 
 let palletG = document.getElementsByClassName('color')[3]
 palletG.style.backgroundColor = 'green'
+
+function setColor(){
+    let R = 0;
+    let G = 0;
+    let B = 0;
+    R = Math.floor(Math.random() * 255);
+    G = Math.floor(Math.random() * 255);
+    B = Math.floor(Math.random() * 255);
+    return 'rgb' + '(' + R + ', ' +  G + ', ' + R + ')';
+}
+function alteraCor() {
+    palletR.style.backgroundColor = setColor()
+    palletB.style.backgroundColor = setColor()
+    palletG.style.backgroundColor = setColor()
+    
+} 
+
+let button = document.getElementById('button-random-color');
+
+button.addEventListener('click', alteraCor);
