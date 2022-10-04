89dec38fb841ddb32573911502bfbac690b7f4f5
JFladsonSIlva <fladsonsilva1@gmail.com>
2022-10-04 16:58:12 -0300

    adicionado cores no JS

---
+++ script.js
@@ -0,0 +1,8 @@
+let palletR = document.getElementsByClassName('color')[1]
+palletR.style.backgroundColor = 'red'
+
+let palletB = document.getElementsByClassName('color')[2]
+palletB.style.backgroundColor = 'blue'
+
+let palletG = document.getElementsByClassName('color')[3]
+palletG.style.backgroundColor = 'green'
