1f2d23a8c5229fa7d9659eaf7bdb3a724a32b77e
JFladsonSIlva <fladsonsilva1@gmail.com>
2022-10-10 09:34:16 -0300

    iniciando

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 window.onload = function (){
     // createPixels()
+
+    function recuperaCor() {
+        let arr = localStorage.getItem('RGBcolor')
+        if (localStorage = arr) {
+            palletR.style.backgroundColor = arr
+            palletB.style.backgroundColor = arr
+            palletG.style.backgroundColor = arr
+            console.log(arr)
+        }
+    }
+    recuperaCor()
 }
 
 let palletR = document.getElementsByClassName('color')[1]
@@ -18,22 +29,46 @@ function RGBgenerator(){
     R = Math.floor(Math.random() * 255);
     G = Math.floor(Math.random() * 255);
     B = Math.floor(Math.random() * 255);
-    return 'rgb' + '(' + R + ', ' +  G + ', ' + R + ')';
+    let RGB = 'rgb' + '(' + R + ', ' +  G + ', ' + R + ')';
+    return RGB;
+    
+//REQUISITO 5; Preciso achar uma forma de atribuir o valor do retorno da função para armazenar no localStorage. 
     
 }
 
+
 function setColor() {
-    palletR.style.backgroundColor = RGBgenerator()
-    palletB.style.backgroundColor = RGBgenerator()
-    palletG.style.backgroundColor = RGBgenerator()
+     let R = RGBgenerator()
+     let G = RGBgenerator()
+     let B = RGBgenerator()
+     palletR.style.backgroundColor = R
+     
+     palletB.style.backgroundColor = G
+     palletG.style.backgroundColor = B
+     localStorage.setItem('RGBcolor', 'rgb' + '(' + R + ', ' +  G + ', ' + R + ')')
+     console.log();
+   
+   
 
 } 
+
+// function adicionaItensStorage() {
+//     const itens = RGBgenerator
+//     let arrItens = [];
+//     for (const item of itens) {
+//       arrItens.push(item.innerText);
+//     }
+//     addStorage('itens', arrItens);
+//   }
+  
+
+// function addStorage(key, value) {
+//     localStorage.setItem(key, JSON.stringify(value));
+//   }
+
+
 // localStorage.setItem('RGBcolor', (rgb))
-console.log(RGBgenerator())
-
-
-
-
+// console.log(RGBgenerator())
 
 let button = document.getElementById('button-random-color');
 
