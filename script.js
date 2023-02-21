046d5fff2143fb5ca37561f2749d50c6ec71fefb
JFladsonSIlva <fladsonsilva1@gmail.com>
2023-02-21 16:03:34 -0300

    estilizado e btn de clear criado

--- script.js
+++ script.js
@@ -1,147 +1,125 @@
-window.onload = function () {
-//REQUISITO 5
-    function recuperaCor() {
-        let arr = JSON.parse(localStorage.getItem('colorPalette'));
-        if (localStorage = arr) {
-            palletR.style.backgroundColor = arr[0]
-            palletB.style.backgroundColor = arr[1]
-            palletG.style.backgroundColor = arr[2]
-        }
-    }
-    recuperaCor()
+const palletR = document.getElementsByClassName('color')[1];
+palletR.style.backgroundColor = 'red';
 
+const palletB = document.getElementsByClassName('color')[2];
+palletB.style.backgroundColor = 'blue';
 
-mudaCores()
+const palletG = document.getElementsByClassName('color')[3];
+palletG.style.backgroundColor = 'green';
 
+const pallet1 = document.getElementsByClassName('black')[0];
+pallet1.style.backgroundColor = 'black';
+
+// requisito 8
+function mudaCores() {
+  const pixels = document.querySelectorAll('.pixel');
+
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].addEventListener('click', () => {
+      const corSelected = document.querySelector('.selected');
+      pixels[i].style.backgroundColor = corSelected.style.backgroundColor;
+    });
+  }
 }
 
+window.onload = () => {
+// REQUISITO 5
+  function recuperaCor() {
+    const localPalletColor = JSON.parse(localStorage.getItem('colorPalette'));
+    if (localPalletColor) {
+      const [color1, color2, color3] = localPalletColor;
+      palletR.style.backgroundColor = color1;
+      palletB.style.backgroundColor = color2;
+      palletG.style.backgroundColor = color3;
+    }
+  }
+  recuperaCor();
+  mudaCores();
+};
+
 // VARIAVEIS COM AS CORES DOS QUADRINHOS DA PALETA
-let palletR = document.getElementsByClassName('color')[1]
-palletR.style.backgroundColor = 'red'
-
-let palletB = document.getElementsByClassName('color')[2]
-palletB.style.backgroundColor = 'blue'
-
-let palletG = document.getElementsByClassName('color')[3]
-palletG.style.backgroundColor = 'green'
-
-let  pallet1 = document.getElementsByClassName('black')[0]
-pallet1.style.backgroundColor = 'black'
 
 // FUNÇÃO PARA GERAR CORES
 function RGBgenerator() {
-    let R = Math.floor(Math.random() * 256);
-    let G = Math.floor(Math.random() * 256);
-    let B = Math.floor(Math.random() * 256);
+  const R = Math.floor(Math.random() * 256);
+  const G = Math.floor(Math.random() * 256);
+  const B = Math.floor(Math.random() * 256);
 
-    let RGB = `rgb(${R}, ${G}, ${B})`;
-    return RGB;
+  const RGB = `rgb(${R}, ${G}, ${B})`;
+
+  return RGB;
 }
 // FUNÇÃO PARA SETAR A COR
 function setColor() {
-    let R = palletR.style.backgroundColor = RGBgenerator()
-    let G = palletB.style.backgroundColor = RGBgenerator()
-    let B = palletG.style.backgroundColor = RGBgenerator()
+//   const R = palletR.style.backgroundColor = RGBgenerator()
+//   const G = palletB.style.backgroundColor = RGBgenerator()
+//   const B = palletG.style.backgroundColor = RGBgenerator()
+  //   const armazenaCor = [R, G, B];
+  const armazenaCor = [
+    palletR.style.backgroundColor = RGBgenerator(),
+    palletB.style.backgroundColor = RGBgenerator(),
+    palletG.style.backgroundColor = RGBgenerator(),
+  ];
 
-    let armazenaCor = [R, G, B]
-
-    localStorage.setItem('colorPalette', JSON.stringify(armazenaCor))
+  localStorage.setItem('colorPalette', JSON.stringify(armazenaCor));
 }
 // EVENTLISTINER PARA AS CORES ALEATÓRIAS
-let button = document.getElementById('button-random-color');
+const button = document.getElementById('button-random-color');
 button.addEventListener('click', setColor);
 
-//REQUISITO 6
+// REQUISITO 6
 function geraPixer() {
-    let quadro = document.getElementById('pixel-board')
+  const quadro = document.getElementById('pixel-board');
 
-    for (let i = 0; i < 25; i += 1) {
-        let pixel = document.createElement('div');
-        pixel.classList.add('pixel');
-        quadro.appendChild(pixel);
-    }
-    return quadro;
+  for (let i = 0; i < 25; i += 1) {
+    const pixel = document.createElement('div');
+    pixel.classList.add('pixel');
+    quadro.appendChild(pixel);
+  }
+  return quadro;
 }
-geraPixer()
+geraPixer();
 
-function pintora(){
-    let pixels = document.getElementsByClassName
-}
-//requisito 8
-function mudaCores(){
-    let pixels = document.querySelectorAll('.pixel')
-    
-    // console.log(corSelected)
-    for(let i = 0; i < pixels.length; i += 1){
-        pixels[i].addEventListener('click', () => {
-            let corSelected = document.querySelector('.selected')
-            pixels[i].style.backgroundColor = corSelected.style.backgroundColor;
-            console.log(corSelected)
-        })
-    }
+// REQUISITO 9 e 10
+function removeClass() {
+  const selected = document.querySelector('.selected');
+  selected.classList.remove('selected');
 }
 
-    //REQUISITO 9 e 10
-    function removeClass() {
-        let selected = document.querySelector('.selected')
-        selected.classList.remove('selected')
-    }
-   
-    pallet1.addEventListener('click', () => {
-        removeClass()
-        pallet1.classList.add('selected')
-        console.log('black');
-    })
-    
-    palletR.addEventListener('click', () => {
-        removeClass()
-        palletR.classList.add('selected')
-        console.log('red');
-    })
-    
-    palletB.addEventListener('click', () => {
-        removeClass()
-        palletB.classList.add('selected')
-        console.log('blue');
-    })
-    
-    palletG.addEventListener('click', () => {
-        removeClass()
-        palletG.classList.add('selected')
-        console.log('green');
-    })
-    // -----------------------------------------------------
+pallet1.addEventListener('click', () => {
+  removeClass();
+  pallet1.classList.add('selected');
+  console.log('black');
+});
 
-    // pallet1.addEventListener('click', () => {
-    //     pallet1.classList.add('selected')
-    //     palletR.classList.remove('selected')
-    //     palletG.classList.remove('selected')
-    //     palletB.classList.remove('selected')
-    //     console.log('black');
-    // })
-    
-    // palletR.addEventListener('click', () => {
-    //     palletR.classList.add('selected')
-    //     pallet1.classList.remove('selected')
-    //     palletG.classList.remove('selected')
-    //     palletB.classList.remove('selected')
-    //     console.log('red');
-    // })
-    
-    // palletB.addEventListener('click', () => {
-    //     palletB.classList.add('selected')
-    //     palletR.classList.remove('selected')
-    //     palletG.classList.remove('selected')
-    //     pallet1.classList.remove('selected')
-    //     console.log('blue');
-    // })
-    
-    // palletG.addEventListener('click', () => {
-    //     palletG.classList.add('selected')
-    //     palletR.classList.remove('selected')
-    //     pallet1.classList.remove('selected')
-    //     palletB.classList.remove('selected')
-    //     console.log('green');
-    // })
-    // // -
+palletR.addEventListener('click', () => {
+  removeClass();
+  palletR.classList.add('selected');
+  // console.log('red');
+});
 
+palletB.addEventListener('click', () => {
+  removeClass();
+  palletB.classList.add('selected');
+//   console.log('blue');
+});
+
+palletG.addEventListener('click', () => {
+  removeClass();
+  palletG.classList.add('selected');
+// console.log('green');
+});
+
+// Requiisito 11
+const clearButton = document.getElementById('clear-board');
+
+const clearBoard = () => {
+  const pixels = document.querySelectorAll('.pixel');
+
+  pixels.forEach((pixel) => {
+    const varPixel = pixel;
+    varPixel.style.backgroundColor = '#fff';
+  });
+};
+
+clearButton.addEventListener('click', clearBoard);
