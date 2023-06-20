1f1dda9ab32dee6f55329eb0767c6b483e5ed50e
JFladsonSIlva <fladsonsilva1@gmail.com>
2023-06-20 19:39:38 -0300

    Last details

--- script.js
+++ script.js
@@ -10,10 +10,18 @@ palletG.style.backgroundColor = 'green';
 const pallet1 = document.getElementsByClassName('black')[0];
 pallet1.style.backgroundColor = 'black';
 
+// apaga pixels existentes -----------------------------------------------------
+const removePixels = () => {
+  const quadro = document.getElementById('pixel-board');
+  const pixels = document.querySelectorAll('.pixel');
+  for (let i = 0; i < pixels.length; i += 1) {
+    quadro.removeChild(pixels[i]);
+  }
+};
+
 // requisito 8
 function mudaCores() {
   const pixels = document.querySelectorAll('.pixel');
-
   for (let i = 0; i < pixels.length; i += 1) {
     pixels[i].addEventListener('click', () => {
       const corSelected = document.querySelector('.selected');
@@ -21,6 +29,22 @@ function mudaCores() {
     });
   }
 }
+// requisito 15 abaixo
+const recuperaBoardSize = () => {
+  const boardSize = JSON.parse(localStorage.getItem('boardSize'));
+  if (boardSize) {
+    const quadro = document.getElementById('pixel-board');
+    removePixels();
+    for (let i = 0; i < boardSize * boardSize; i += 1) {
+      const pixel = document.createElement('div');
+      pixel.classList.add('pixel');
+      quadro.appendChild(pixel);
+    }
+    // mudança, tirei o tamanh doo quadro do CSS e coloquei aqui no Javascript
+    quadro.style.height = boardSize * 44 + 'px';
+    quadro.style.width = boardSize * 45 + 'px';
+  }
+};
 
 window.onload = () => {
 // REQUISITO 5
@@ -33,6 +57,7 @@ window.onload = () => {
       palletG.style.backgroundColor = color3;
     }
   }
+  recuperaBoardSize();
   recuperaCor();
   mudaCores();
 };
@@ -49,6 +74,7 @@ function RGBgenerator() {
 
   return RGB;
 }
+console.log(RGBgenerator());
 // FUNÇÃO PARA SETAR A COR
 function setColor() {
 //   const R = palletR.style.backgroundColor = RGBgenerator()
@@ -67,18 +93,30 @@ function setColor() {
 const button = document.getElementById('button-random-color');
 button.addEventListener('click', setColor);
 
-// REQUISITO 6
+// REQUISITO 6 criar o quadro de pixels iniciais
+
 function geraPixer() {
   const quadro = document.getElementById('pixel-board');
-
   for (let i = 0; i < 25; i += 1) {
     const pixel = document.createElement('div');
     pixel.classList.add('pixel');
     quadro.appendChild(pixel);
   }
+  // mudança, tirei o tamanh doo quadro do CSS e coloquei aqui no Javascript
+  quadro.style.width = '220px';
+  quadro.style.height = '220px';
   return quadro;
 }
 geraPixer();
+// const quadro = document.getElementById('pixel-board');
+// for (let i = 0; i < 25; i += 1) {
+//   const pixel = document.createElement('div');
+//   pixel.classList.add('pixel');
+//   quadro.appendChild(pixel);
+// }
+// mudança, tirei o tamanh doo quadro do CSS e coloquei aqui no Javascript
+// quadro.style.width = '220px';
+// quadro.style.height = '220px';
 
 // REQUISITO 9 e 10
 function removeClass() {
@@ -123,3 +161,44 @@ const clearBoard = () => {
 };
 
 clearButton.addEventListener('click', clearBoard);
+
+// req 13 mudar o a quantidade de pixels dinamicamente
+
+// ---------------------------------------------------------------------------------
+const btnBoardSize = document.getElementById('generate-board');
+
+const verifyNumberInput = (value) => {
+  let valueValid = 0;
+  if (value < 5) {
+    valueValid = 5;
+  } else if (value > 50) {
+    valueValid = 50;
+  } else {
+    valueValid = value;
+  }
+  return valueValid;
+};
+
+const changeFrameSize = () => {
+  const inputBoardSize = document.getElementById('board-size');
+  const quadro = document.getElementById('pixel-board');
+  if (Number(inputBoardSize.value) <= 0) {
+    alert('Board inválido!');
+  }
+  const imputValue = verifyNumberInput(inputBoardSize.value);
+  removePixels();
+  for (let i = 0; i < imputValue * imputValue; i += 1) {
+    const pixel = document.createElement('div');
+    pixel.classList.add('pixel');
+    quadro.appendChild(pixel);
+    quadro.style.height = imputValue * 44 + 'px';
+    quadro.style.width = imputValue * 45 + 'px';
+  }
+  localStorage.setItem('boardSize', JSON.stringify(imputValue));
+  mudaCores();
+};
+
+btnBoardSize.addEventListener('click', changeFrameSize);
+
+// notas
+// talvez eu possa criar uma função para apagar os pixels existentes
